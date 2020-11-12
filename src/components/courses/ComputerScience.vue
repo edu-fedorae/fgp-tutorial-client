9076fa22b87541b7c9b65cf285c486754830805b
froshims <lazarusgrey37@gmail.com>
2020-11-11 22:32:43 -0800

    better looking courses on home page

---
+++ src/components/courses/ComputerScience.vue
@@ -0,0 +1,96 @@
+<template>
+  <v-container>
+    <h3>Computer Science - :</h3>
+    <v-row align="left" dense>
+      <v-col v-for="(i, c) in 3" :key="i" class="d-inline ma-1">
+        <v-card :loading="loading" max-width="374" elevation="12" dense>
+          <template slot="progress">
+            <v-progress-linear
+              color="deep-purple"
+              height="10"
+              indeterminate
+            ></v-progress-linear>
+          </template>
+
+          <v-img
+            height="250"
+            src="https://cdn.vuetifyjs.com/images/cards/cooking.png"
+          ></v-img>
+
+          <v-card-title>Intro to Computer Science</v-card-title>
+
+          <v-card-text>
+            <v-row align="left" class="mx-0">
+              <v-rating
+                :value="4.5"
+                color="amber"
+                dense
+                half-increments
+                readonly
+                size="14"
+              ></v-rating>
+
+              <div class="grey--text ml-4">
+                4.5 (413)
+              </div>
+            </v-row>
+
+            <div>
+              Small plates, salads & sandwiches - an intimate setting with 12
+              indoor seats plus patio seating.
+            </div>
+          </v-card-text>
+
+          <p v-show="false">{{ c }}</p>
+
+          <v-divider class="mx-4"></v-divider>
+
+          <v-card-title>Available at</v-card-title>
+
+          <v-card-text>
+            <v-chip-group
+              v-model="selection"
+              active-class="deep-purple accent-4 white--text"
+              column
+            >
+              <v-chip>5:30AM</v-chip>
+
+              <v-chip>7:30AM</v-chip>
+
+              <v-chip>8:00AM</v-chip>
+
+              <v-chip>9:00AM</v-chip>
+            </v-chip-group>
+          </v-card-text>
+
+          <v-card-actions>
+            <v-btn color="deep-purple lighten-2" text @click="reserve">
+              Reserve
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "ComputerScience.vue",
+  data() {
+    return {
+      loading: false,
+      selection: 1
+    };
+  },
+  methods: {
+    reserve() {
+      this.loading = true;
+
+      setTimeout(() => (this.loading = false), 2000);
+    }
+  }
+};
+</script>
+
+<style scoped></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,199 +2,31 @@
   <div class="home">
     <SignIn />
     <v-divider></v-divider>
-    <v-main>
-      <div class="courses">
-        <v-card class="mx-auto elevation-10" outlined>
-          <v-container class="fill-height" fluid>
-            <h1>Computer Science</h1>
-            <v-row align="left" justify="left">
-              <v-col cols="12" sm="8" md="4" v-for="(i, n) in 3" :key="i">
-                <v-card
-                  class="elevation-12"
-                  max-width="344"
-                >
-                  <v-img
-                    src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
-                    height="200px"
-                  ></v-img>
-
-                  <v-card-title>
-                    Top western road trips - {{ n }}
-                  </v-card-title>
-
-                  <v-card-subtitle>
-                    1,000 miles of wonder
-                  </v-card-subtitle>
-
-                  <v-card-actions>
-                    <v-btn
-                      color="orange lighten-2"
-                      text
-                    >
-                      Explore
-                    </v-btn>
-
-                    <v-spacer></v-spacer>
-
-                    <v-btn
-                      icon
-                      @click="show = !show"
-                    >
-                      <v-icon>{{ show ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
-                    </v-btn>
-                  </v-card-actions>
-
-                  <v-expand-transition>
-                    <div v-show="show">
-                      <v-divider></v-divider>
-
-                      <v-card-text>
-                        I'm a thing. But, like most politicians, he promised more than he could deliver. You won't have time for sleeping, soldier, not with all the bed making you'll be doing. Then we'll go with that data file! Hey, you add a one and two zeros to that or we walk! You're going to do his laundry? I've got to find a way to escape.
-                      </v-card-text>
-                    </div>
-                  </v-expand-transition>
-                </v-card>
-              </v-col>
-            </v-row>
-          </v-container>
-        </v-card>
+    <v-container>
+      <h1>Courses</h1>
+      <div>
+        <ComputerScience />
       </div>
-
-      <div class="courses">
-        <v-card class="mx-auto elevation-10" outlined>
-          <v-container class="fill-height" fluid>
-            <h1>Psychology</h1>
-            <v-row align="left" justify="left">
-              <v-col cols="12" sm="8" md="4" v-for="(i, n) in 3" :key="i">
-                <v-card
-                  class="elevation-12"
-                  max-width="344"
-                >
-                  <v-img
-                    src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
-                    height="200px"
-                  ></v-img>
-
-                  <v-card-title>
-                    Top western road trips - {{ n }}
-                  </v-card-title>
-
-                  <v-card-subtitle>
-                    1,000 miles of wonder
-                  </v-card-subtitle>
-
-                  <v-card-actions>
-                    <v-btn
-                      color="orange lighten-2"
-                      text
-                    >
-                      Explore
-                    </v-btn>
-
-                    <v-spacer></v-spacer>
-
-                    <v-btn
-                      icon
-                      @click="show = !show"
-                    >
-                      <v-icon>{{ show ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
-                    </v-btn>
-                  </v-card-actions>
-
-                  <v-expand-transition>
-                    <div v-show="show">
-                      <v-divider></v-divider>
-
-                      <v-card-text>
-                        I'm a thing. But, like most politicians, he promised more than he could deliver. You won't have time for sleeping, soldier, not with all the bed making you'll be doing. Then we'll go with that data file! Hey, you add a one and two zeros to that or we walk! You're going to do his laundry? I've got to find a way to escape.
-                      </v-card-text>
-                    </div>
-                  </v-expand-transition>
-                </v-card>
-              </v-col>
-            </v-row>
-          </v-container>
-        </v-card>
-      </div>
-
-      <div class="courses">
-        <v-card class="mx-auto elevation-10" outlined>
-          <v-container class="fill-height" fluid>
-            <h1>Business</h1>
-            <v-row align="left" justify="left">
-              <v-col cols="12" sm="8" md="4" v-for="(i, n) in 3" :key="i">
-                <v-card
-                  class="elevation-12"
-                  max-width="344"
-                >
-                  <v-img
-                    src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
-                    height="200px"
-                  ></v-img>
-
-                  <v-card-title>
-                    Top western road trips - {{ n }}
-                  </v-card-title>
-
-                  <v-card-subtitle>
-                    1,000 miles of wonder
-                  </v-card-subtitle>
-
-                  <v-card-actions>
-                    <v-btn
-                      color="orange lighten-2"
-                      text
-                    >
-                      Explore
-                    </v-btn>
-
-                    <v-spacer></v-spacer>
-
-                    <v-btn
-                      icon
-                      @click="show = !show"
-                    >
-                      <v-icon>{{ show ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
-                    </v-btn>
-                  </v-card-actions>
-
-                  <v-expand-transition>
-                    <div v-show="show">
-                      <v-divider></v-divider>
-
-                      <v-card-text>
-                        I'm a thing. But, like most politicians, he promised more than he could deliver. You won't have time for sleeping, soldier, not with all the bed making you'll be doing. Then we'll go with that data file! Hey, you add a one and two zeros to that or we walk! You're going to do his laundry? I've got to find a way to escape.
-                      </v-card-text>
-                    </div>
-                  </v-expand-transition>
-                </v-card>
-              </v-col>
-            </v-row>
-          </v-container>
-        </v-card>
-      </div>
-    </v-main>
+    </v-container>
   </div>
 </template>
 
 <script>
 import SignIn from "@/components/home/SignIn";
+import ComputerScience from "@/components/courses/ComputerScience";
 
 export default {
   name: "Home",
   components: {
+    ComputerScience,
     SignIn
   },
   data() {
     return {
       show: false
-    }
+    };
   }
 };
 </script>
 
-<style scoped>
-  .courses {
-    margin: 2rem;
-  }
-</style>
+<style scoped></style>
