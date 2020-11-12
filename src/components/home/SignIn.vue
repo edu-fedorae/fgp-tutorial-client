d6ae0b24650bcd010c491a8ccc0b37e6c0db4ba0
froshims <lazarusgrey37@gmail.com>
2020-11-12 01:46:37 -0800

    Attempt to remove space of home page

--- src/components/home/SignIn.vue
+++ src/components/home/SignIn.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-container class="fill-height" fluid>
+  <v-container>
     <v-row align="center" justify="center">
       <v-col cols="12" sm="8" md="4">
         <v-card class="elevation-12">
