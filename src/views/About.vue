f365df7fee12881517f45fe08bd147d8b50eb473
Fedorae SW <55074858+Aeorae@users.noreply.github.com>
2021-07-23 00:35:10 +0100

    Create About.vue

---
+++ src/views/About.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="about-us">
+    <v-container>
+      <h1>About Us</h1>
+      <div class="">
+ 
+      </div>
+    </v-container>
+  </div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    window.scrollTo(0, 0);
+  }
+};
+</script>
+
+<style lang="css" scoped></style>
